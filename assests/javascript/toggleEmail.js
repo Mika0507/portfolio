463a30f32f2c3f07ebb492cff08a5595e4fa8f39
Belén <alonsoperalbelen@gmail.com>
2024-04-15 19:04:43 +0200

    about page already finish

---
+++ assests/javascript/toggleEmail.js
@@ -0,0 +1,8 @@
+function toggleEmail() {
+    let email = document.getElementById("email");
+    if (email.style.display === "none") {
+      email.style.display = "block";
+    } else {
+      email.style.display = "none";
+    }
+  }

ac6e609c53c531acd069115be505435b505d147f
Belén <alonsoperalbelen@gmail.com>
2024-04-24 10:53:51 +0200

    page about the email is completed hidden

--- assests/javascript/toggleEmail.js
+++ assests/javascript/toggleEmail.js
@@ -1,8 +1,12 @@
+let emailAddress = 'alonsoperalbelen@gmail.com'
+
 function toggleEmail() {
     let email = document.getElementById("email");
     if (email.style.display === "none") {
+      email.textContent = emailAddress;
       email.style.display = "block";
     } else {
+      email.textContent = '';
       email.style.display = "none";
     }
   }
